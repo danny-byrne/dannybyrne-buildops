3ca8a1c09f9a235b234f42230a2977c538938045
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-12-04 13:45:27 -0800

    stylesheet mobile first refactor

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -13,20 +13,18 @@ import Employee from "./Employee";
 export default function Employees(props) {
   const { employees } = props;
   return (
-    <div>
-      <div id="EmployeesContainer">
-        {employees.map((e) => (
-          <Employee
-            key={e.id}
-            id={e.id}
-            firstName={e.firstname}
-            lastName={e.lastname}
-            skills={e.skills}
-            skillHandlers={props.skillHandlers}
-            employeeHandlers={props.employeeHandlers}
-          />
-        ))}
-      </div>
+    <div id="EmployeesContainer">
+      {employees.map((e) => (
+        <Employee
+          key={e.id}
+          id={e.id}
+          firstName={e.firstname}
+          lastName={e.lastname}
+          skills={e.skills}
+          skillHandlers={props.skillHandlers}
+          employeeHandlers={props.employeeHandlers}
+        />
+      ))}
     </div>
   );
 }
